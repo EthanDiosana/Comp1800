0c132162efb17bee1d70cec00ddc6e39f3bd92fa
Ethan Diosana <danielrenendez@gmail.com>
2021-04-09 11:01:46 -0700

    added alerts

---
+++ scripts/un_imp_feats.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    console.log("un_imp_feats.js is functioning.");
+
+
+    //BUTTON CLICK FUNCTION
+    /*function createbutton(buttonID) {
+        $("#" + buttonID).click(
+            function () {
+                alert("Unimplemented Feature.");
+            }
+        );
+    }*/
+
+
+    $('.active').click(
+        function () {
+            console.log("Button clicked.");
+            alert("Unimplemented Feature.");
+        }
+    );
+})
